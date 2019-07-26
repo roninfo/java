f2d9d15c683105e4b935bb2c2f79322b27e4df22
Roninfo <roninfo@gmail.com>
2019-07-26 19:58:11 -0300

    sherlock anagrams and reader file with streams

--- src/main/java/hackerrank/SherlockAndAnagrams.java
+++ src/main/java/hackerrank/SherlockAndAnagrams.java
@@ -1,4 +1,41 @@
 package hackerrank;
 
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class SherlockAndAnagrams {
+
+    @Test
+    public void init() {
+        String s = "ifailuhkqq";
+        sherlockAndAnagrams(s);
+    }
+
+    private void sherlockAndAnagrams(String str) {
+        HashMap<String, Integer> map = new HashMap<>();
+        int count = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            for (int j = i + 1; j <= str.length(); j++) {
+                char[] subChars = str.substring(i, j).toCharArray();
+                Arrays.sort(subChars);
+                String restSub = String.valueOf(subChars);
+
+                map.put(restSub, map.getOrDefault(restSub, 0) + 1);
+            }
+        }
+
+        for (int i : map.values()) {
+            if (i == 2) {
+                count++;
+            } else if (i > 2) {
+                int x = i*(i-1)/2;
+                count+=x;
+            }
+        }
+        System.out.println(count);
+    }
 }
--- src/main/java/util/GetFileFromResources.java
+++ src/main/java/util/GetFileFromResources.java
@@ -1,15 +1,20 @@
 package util;
 
 import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
 
 public class GetFileFromResources {
 
     public static GetFileFromResources INSTANCE = new GetFileFromResources();
 
-    public File getFileFromResources(String fileName) {
+    final ClassLoader classLoader = getClass().getClassLoader();
 
-        ClassLoader classLoader = getClass().getClassLoader();
+    public File getFileFromResources(String fileName) {
 
         URL resource = classLoader.getResource(fileName);
         if (resource == null) {
@@ -17,6 +22,17 @@ public class GetFileFromResources {
         } else {
             return new File(resource.getFile());
         }
+    }
+
+    public Stream<String> getLineFileWithStream(String fileName) {
+        URL resource = classLoader.getResource(fileName);
+        Stream<String> lines = null;
+        try {
+            lines = Files.lines(Paths.get(resource.toURI()));
+        } catch (IOException | URISyntaxException e) {
+            e.printStackTrace();
+        }
+        return lines;
     }
 
 }
